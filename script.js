8ca664c41f7155fc44eea699dea9f1f34e90debf
RanjeeshTech <ranjeeshr07012002@gmail.com>
2021-07-25 15:48:40 +0530

    first

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
 /*Fill your code */
+var count=1;
+var chocolate = function(){
+    for(let i=0;i<6;i++){
+        if(count==i){
+            document.getElementById("layer"+i).style.visibility="visible";
+            document.getElementById("layer"+i).style.backgroundColor="#7B3F00";
+            document.getElementById("chocolate").style.display="block";
+        }
+    }
+    count++;
+}
+var strawberry = function(){
+    for(let i=0;i<6;i++){
+        if(count==i){
+            document.getElementById("layer"+i).style.visibility="visible";
+            document.getElementById("layer"+i).style.backgroundColor=" #fc5a8d";
+            document.getElementById("strawberry").style.display="block";
+        }
+    }
+    count++;
+}
+var butterscoth = function(){
+    for(let i=0;i<6;i++){
+        if(count==i){
+            document.getElementById("layer"+i).style.visibility="visible";
+            document.getElementById("layer"+i).style.backgroundColor="#E3963E";
+            document.getElementById("butterscoth").style.display="block";
+        }
+    }
+    count++;
+}
+var vanilla = function(){
+    for(let i=0;i<6;i++){
+        if(count==i){
+            document.getElementById("layer"+i).style.visibility="visible";
+            document.getElementById("layer"+i).style.backgroundColor="#F3E5AB";
+            document.getElementById("vanilla").style.display="block";
+        }
+    }
+    count++;
+}
+var redvelvet = function(){
+    for(let i=0;i<6;i++){
+        if(count==i){
+            document.getElementById("layer"+i).style.visibility="visible";
+            document.getElementById("layer"+i).style.backgroundColor="#9c0000";
+            document.getElementById("redvelvet").style.display="block";
+        }
+    }
+    count++;
+}
